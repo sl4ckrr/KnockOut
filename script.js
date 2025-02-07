e05f81ac62d1ca8531945b806399021e6fb9ada4
Rhap5ody <domingogramos808@gmail.com>
2025-02-07 09:50:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// similar behavior as an HTTP redirect
+window.location.replace("https://sl4ckrr.github.io./KnockOut/lobby");
 
+// similar behavior as clicking on a link
+window.location.replace("https://sl4ckrr.github.io./KnockOut/lobby");
