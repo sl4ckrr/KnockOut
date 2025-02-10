330169af7afe8bd679d755ec09b94f20799f55b6
Rhap5ody <domingogramos808@gmail.com>
2025-02-10 10:04:09 -0600

    Update knockout-script.js

--- src/knockout-script.js
+++ src/knockout-script.js
@@ -1,5 +1,6 @@
 // similar behavior as an HTTP redirect
-window.location.replace("https://sl4ckrr.github.io./KnockOut/lobby.html");
+window.location.replace("https://sl4ckrr.github.io./KnockOut/lobby.html"); 
+
 
 // similar behavior as clicking on a link
 window.location.replace("https://sl4ckrr.github.io./KnockOut/lobby.html");
