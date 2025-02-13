f0848ce0771bd7af0e2fc0b37b317015c0b929b8
Rhap5ody <domingogramos808@gmail.com>
2025-02-13 14:42:59 -0600

    Update overworld.js

--- overworld.js
+++ overworld.js
@@ -11,45 +11,20 @@ const image = New Image();
     this.ctx.drawimage(image,0,0)
   };
   image.src = "img/"; // map img
-  
 
-const x = 5;
-const y = 6;
+// place some game objects
+  const hero = new GameObject({
+    x: 5,
+    y: 6,
+  })  
+    const npc1 = new GameObject({
+    x: 7,
+    y: 9,
+      src: "images/characters/people/npc1.png";
+  })
 
-  const shadow = new image();
-    shadow.onload = () =>{
-          this.ctx.drawimage(
-      shadow,
-      0, // left cut
-      0, // top cut,
-      32, //width of cut
-      32, //height of cut
-      x * 16 - 8,
-      y * 16 - 18,
-      32,
-      32,
-    ) 
-    }
-shadow.src = ""; // shadow img
-  
-  
-const hero = New Image();
-  hero.onload = () =>{
-    this.ctx.drawimage(
-      hero,
-      0, // left cut
-      0, // top cut,
-      32, //width of cut
-      32, //height of cut
-      x * 16 - 8,
-      y * 16 - 18,
-      32,
-      32,
-    )
-  };
-  }
-  hero.src = "spritesheet/hero.png"; // main character img
-
+hero.sprite.draw(this.ctx);
+npc1.sprite.draw(this.ctx);
   
  } 
 
