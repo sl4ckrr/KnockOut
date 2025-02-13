338f72d69796fe2b9991a2d716dbc885f03a8daa
Rhap5ody <domingogramos808@gmail.com>
2025-02-13 04:12:24 -0600

    Update overworld.js

--- overworld.js
+++ overworld.js
@@ -6,12 +6,35 @@ costructor(config) {
 }
 
 init() {
-const image = New image();
+const image = New Image();
   image.onload = () =>{
     this.ctx.drawimage(image,0,0)
   };
   image.src = "img/";
-}
+  
+
+const x = 0;
+const y = 0;
 
   
+const hero = New Image();
+  hero.onload = () =>{
+    this.ctx.drawimage(
+      hero,
+      0, // left cut
+      0, // top cut,
+      32, //width of cut
+      32, //height of cut
+      x,
+      y,
+      32,
+      32,
+    )
+  };
+  }
+  hero.src = "img/";
+
+  
+ } 
+
 }
