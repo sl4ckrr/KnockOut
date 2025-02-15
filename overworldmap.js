becc1109c498939e08616a0384dbc46d19838151
Rhap5ody <domingogramos808@gmail.com>
2025-02-14 20:51:08 -0600

    Update overworldmap.js

--- overworldmap.js
+++ overworldmap.js
@@ -17,3 +17,41 @@ class OverworldMap{
     ctx.drawImage(this.upperImage, 0, 0)
   }
 }
+
+window.OverworldMaps(ctx) = {
+  DemoRoom: {
+    lowerSrc:"images/maps/demoLower.png",
+    upperSrc:"images/maps/demoUpper.png",
+      gameObjects: {
+      hero:new gameObject({
+        x: 5,
+        y: 6,
+      }),
+  npc1:new gameObject({
+        x: 7,
+        y: 9,
+        src:"images/characters/people/npc1.png";
+      })
+    }
+  },
+    LivingRoom: {
+    lowerSrc:"images/maps/livingRoomLower.png",
+    upperSrc:"images/maps/livingRoomUpper.png",
+      gameObjects: {
+      hero:new gameObject({
+        x: 3,
+        y: 1,
+      }),
+  npcA:new gameObject({
+        x: 9,
+        y: 2,
+        src:"images/characters/people/npc2.png";
+      })
+  npcB:new gameObject({
+        x: 10,
+        y: 4,
+        src:"images/characters/people/npc3.png";
+      })
+    }
+  },
+}
