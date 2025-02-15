085210be05bd396ba5bd01426d1e8678538fa7c0
Rhap5ody <domingogramos808@gmail.com>
2025-02-14 20:33:24 -0600

    Create overworldmap.js

---
+++ overworldmap.js
@@ -0,0 +1,19 @@
+class OverworldMap{
+  constructor(config) {
+    this.gameObjects = config.gameObjects;
+
+    this.lowerImage = new Image();
+    this.lowerImage.src = config.lowerSrc; 
+
+    this.upperImage = new Image();
+    this.upperImage.src = config.upperSrc;
+  }
+  
+  drawLowerImage(ctx) {
+    ctx.drawImage(this.lowerImage, 0, 0)
+  }
+  
+  drawUpperImage(ctx) {
+    ctx.drawImage(this.upperImage, 0, 0)
+  }
+}
