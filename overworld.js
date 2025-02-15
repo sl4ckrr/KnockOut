cf0ea7cea869be3908e39f57ddcabd2a269d659b
Rhap5ody <domingogramos808@gmail.com>
2025-02-14 21:04:16 -0600

    Update overworld.js

--- overworld.js
+++ overworld.js
@@ -17,28 +17,7 @@ startGameLoop() {
 init() {
 
 this.startGameLoop();
-  
-const image = New Image();
-  image.onload = () =>{
-    this.ctx.drawimage(image,0,0)
-  };
-  image.src = "img/"; // map img
 
-// place some game objects
-  const hero = new GameObject({
-    x: 5,
-    y: 6,
-  })  
-    const npc1 = new GameObject({
-    x: 7,
-    y: 9,
-      src: "images/characters/people/npc1.png";
-  })
-
-  setTimeout(() => {
-    hero.sprite.draw(this.ctx);
-    npc1.sprite.draw(this.ctx);
-  }, 200)
 
   
  } 
